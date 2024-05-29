889d40988f1cdccafef3461e58b13c94f546fd5d
mhdtrk10 <99149415+mhdtrk10@users.noreply.github.com>
2024-05-29 19:49:48 +0300

    basit bir şekilde firebase kullanarak sohbet etme uygulaması

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,17 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Mehdi Oturak on 22.05.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String
+    let body: String
+        
+    
+}
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,33 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Mehdi Oturak on 23.05.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    @IBOutlet weak var rightImageVİew: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
